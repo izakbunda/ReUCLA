1df18f0d1490ccfe2b387bdee9f54e200deabcbf
Izak Bunda <98503066+izakbunda@users.noreply.github.com>
2023-02-01 10:53:32 -0800

    First commit

---
+++ src/screens/Profile.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+/*
+  -- DOCUMENTATION --
+*/
+const Profile = (props) => {
+    return (
+        <View>
+            <Text>Hello! You're in Profile Screen!</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default Profile;
