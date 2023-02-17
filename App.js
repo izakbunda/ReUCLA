7afae8ea4aa549cffaac60c5893736c6e756ea36
Izak Bunda <98503066+izakbunda@users.noreply.github.com>
2023-02-17 11:13:24 -0800

    Added fonts

--- App.js
+++ App.js
@@ -2,28 +2,39 @@ import * as React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 // Import all screens here
 import Home from "./src/screens/Home";
 import Profile from "./src/screens/Profile";
+import Landing from "./src/screens/Landing";
 //
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function NavBarVisible() {
+    return (
+        <Tab.Navigator headerMode="false">
+            <Tab.Screen name="Home" component={Landing} />
+            <Tab.Screen name="Explore" component={Home} />
+            <Tab.Screen name="Profile" component={Profile} />
+        </Tab.Navigator>
+    );
+}
 
 const App = () => {
     return (
         <NavigationContainer>
-            <View style={styles.container}>
-                {/* <Home /> */}
-                {/* <Profile /> */}
-                {/* <Stack.Navigator >
-                    <Stack.Screen
-                        name="Home"
-                        component={Home}
-                        options={{ title: "Welcome" }}
-                    />
-                    <Stack.Screen name="Profile" component={Profile} />
-                </Stack.Navigator> */}
-            </View>
+            {/* <Home /> */}
+            {/* <Profile /> */}
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false,
+                }}
+            >
+                <Stack.Screen name="Home" component={NavBarVisible} />
+                {/* <Stack.Screen name="Profile" component={Profile} /> */}
+            </Stack.Navigator>
         </NavigationContainer>
     );
 };
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,17 +1,80 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, ScrollView, SafeAreaView } from "react-native";
 
 /*
   -- DOCUMENTATION --
 */
 const Profile = (props) => {
     return (
-        <View>
-            <Text>Hello! You're in Profile Screen!</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1 }}>
+            <ScrollView>
+                <View
+                    style={{
+                        flexDirection: "column",
+                        alignItems: "center",
+                        paddingTop: 30,
+                    }}
+                >
+                    {/* Placeholder until stack navigator works */}
+                    {/* <Text
+                        style={{
+                            padding: 20,
+                            fontSize: 20,
+                            fontWeight: "bold",
+                        }}
+                    >
+                        My Profile
+                    </Text> */}
+
+                    <View
+                        style={{
+                            alignItems: "center",
+                            paddingTop: 60,
+                            height: 150,
+                            width: 150,
+                            borderRadius: 1000,
+                            backgroundColor: "black",
+                        }}
+                    >
+                        <Text style={{ color: "white", flexWrap: true }}>
+                            Profile photo
+                        </Text>
+                    </View>
+
+                    <Text
+                        style={{
+                            padding: 20,
+                            fontSize: 30,
+                            fontWeight: "bold",
+                        }}
+                    >
+                        Izak Bunda
+                    </Text>
+
+                    <View
+                        style={{
+                            borderColor: "black",
+                            borderWidth: 1,
+                            borderRadius: 20,
+                            padding: 20,
+                        }}
+                    >
+                        <Text
+                            style={{
+                                fontSize: 15,
+                            }}
+                        >
+                            Hi! I am a 2nd year UCLA student. 🐻🌟
+                        </Text>
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    // h1: {color}
+});
 
 export default Profile;
